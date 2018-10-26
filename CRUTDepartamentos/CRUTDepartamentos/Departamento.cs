475c63e3cec4a531baf5150c6a2b79364ab01684
jackeline varela vasquez <jackelinevarela2@gmail.com>
2018-10-26 15:13:55 -0600

    Creacion de la clase departamento y parte de la clase departamento

---
+++ CRUTDepartamentos/CRUTDepartamentos/Departamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUTDepartamentos
+{
+    class Departamento
+    {
+        //Crear las propiedades de la clase
+        public int id{ get; set; }
+        public string name { get; set; }
+        public string groupName { get; set; }
+        public DateTime modifiedDate { get; set; }
+
+    }
+}
